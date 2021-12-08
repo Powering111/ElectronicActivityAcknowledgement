6a5e2513a82f683b6486fa6ed3008599151b866e
Powering111 <50047351+Powering111@users.noreply.github.com>
2021-12-08 22:19:38 +0900

    make pug

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const pug = require('pug');
+const path = require('path');
+
 const app = express();
 const port = 80;
+const mainPage = pug.compileFile(path.join(__dirname,'/doc/main.pug'));
 
-app.get('/', function(req,res){
-    res.send("hello world!");
+app.get('/:name', function(req,res){
+    console.log(req.params);
+
+    res.send(mainPage({name:req.params.name}));
 });
 
 app.listen(port,()=>{
     console.log("서버가 시작되었습니다.");
-})
+});
