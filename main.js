1da50537c6bbaaaadcd29bac89c5e8c80d1be687
Powering111 <50047351+Powering111@users.noreply.github.com>
2021-12-09 18:42:47 +0900

    pug extend

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 const port = 80;
-const mainPage = pug.compileFile(path.join(__dirname,'/doc/main.pug'));
+const mainPage = pug.compileFile(path.join(__dirname,'/doc/login.pug'));
 
-app.get('/:name', function(req,res){
+app.get('/', function(req,res){
     console.log(req.params);
 
-    res.send(mainPage({name:req.params.name}));
+    res.send(mainPage({name:'KJH'}));
 });
 
 app.listen(port,()=>{
